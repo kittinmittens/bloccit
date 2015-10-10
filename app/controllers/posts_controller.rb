3a5b3de455b54252b3119c738ae0149c8e2e1640
Daniel Peluso <thedanielpeluso@gmail.com>
2015-10-10 12:55:34 -0700

    checkpoint-35 completed with failing spec

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,30 @@ class PostsController < ApplicationController
   end
 
   def show
+    @posts = Post.find(params[:id])
   end
 
+  def create
+   # #9
+       @post = Post.new
+       @post.title = params[:post][:title]
+       @post.body = params[:post][:body]
+
+   # #10
+       if @post.save
+   # #11
+         flash[:notice] = "Post was saved."
+         redirect_to @post
+       else
+   # #12
+         flash[:error] = "There was an error saving the post. Please try again."
+         render :new
+       end
+     end
+
+
   def new
+    @post = Post.new
   end
 
   def edit
