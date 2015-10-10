e45a78a1597c4f6354d0547d4a6221fd6d2c8a7a
Daniel Peluso <thedanielpeluso@gmail.com>
2015-10-10 15:14:39 -0700

    checkpoint-36 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,5 +31,33 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post as update."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. please try again"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\ "was deleted successfully."
+      redirect_to posts_path
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
+    end
+  end
+
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -19,6 +19,50 @@ let (:my_post) {Post.create!(title: RandomData.random_sentence, body: RandomData
     end
   end
 
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit, {id: my_post.id}
+      expect(response).to have_http_status(:success)
+    end
+
+  it "renders the edit view" do
+    get :edit, {id: my_post.id}
+    expect(response).to render_template :edit
+  end
+
+  it "assigns post to be updated to @post" do
+    get :edit, {id: my_post.id}
+    post_instance = assigns(:post)
+    expect(post_instance.id).to eq my_post.id
+    expect(post_instance.title).to eq my_post.title
+    expect(post_instance.body).to eq my_post.body
+  end
+end
+
+describe "PUT update" do
+     it "updates post with expected attributes" do
+       new_title = RandomData.random_sentence
+       new_body = RandomData.random_paragraph
+
+       put :update, id: my_post.id, post: {title: new_title, body: new_body}
+
+ # #3
+       updated_post = assigns(:post)
+       expect(updated_post.id).to eq my_post.id
+       expect(updated_post.title).to eq new_title
+       expect(updated_post.body).to eq new_body
+     end
+
+     it "redirects to the updated post" do
+       new_title = RandomData.random_sentence
+       new_body = RandomData.random_paragraph
+
+ # #4
+       put :update, id: my_post.id, post: {title: new_title, body: new_body}
+       expect(response).to redirect_to my_post
+     end
+   end
+
   describe "GET new" do
         it "returns http success" do
           get :new
@@ -56,6 +100,20 @@ let (:my_post) {Post.create!(title: RandomData.random_sentence, body: RandomData
           expect(response).to redirect_to Post.last
         end
       end
+
+    describe "DELETE destroy" do
+      it "deletes the post" do
+        delete :destroy, {id: my_post.id}
+        count = Post.where({id: my_post.id}).size
+        expect(count).to eq 0
+      end
+
+      it "redirects to posts index" do
+        delete :destroy, {id: my_post.id}
+
+      expect(response).to redirect_to posts_path
+      end
+    end
 
 
          describe "GET show" do
