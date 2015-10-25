fb9a55cbe46e8afbe7237f5109027ddaf6bb8017
Daniel Peluso <thedanielpeluso@gmail.com>
2015-10-24 17:52:33 -0700

    checkpoint-37a complete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+   # #9
+       @user = User.new
+       @user.name = params[:user][:name]
+       @user.email = params[:user][:email]
+       @user.password = params[:user][:password]
+       @user.password_confirmation = params[:user][:password_confirmation]
+
+   # #10
+       if @user.save
+         flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+         redirect_to root_path
+       else
+         flash[:error] = "There was an error creating your account. Please try again."
+         render :new
+       end
+     end
+end
